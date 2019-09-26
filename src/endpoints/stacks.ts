68616cf76998b9dbda575b2e5f0cd4c23a5c789d
sajadam98 <sajad.vb98@gmail.com>
2019-09-26 00:46:38 -0700

    change url in create stack

--- src/endpoints/stacks.ts
+++ src/endpoints/stacks.ts
@@ -34,7 +34,7 @@ const getStackDefinitionFile = async(host: string, token: string, stackId: strin
 };
 
 const createStack = async (host: string, token: string , data: Object) => {
-    const url = host + '/api/stacks';
+    const url = host + '/api/stacks?method=repository&type=1&endpointId=1';
     const headers = {
         'Content-Type': 'multipart/form-data',
         'Authorization': 'Bearer ' + token
